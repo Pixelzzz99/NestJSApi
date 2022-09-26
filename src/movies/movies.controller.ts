885339228fb2d7efc129e582dba86cf1c0e79aad
Pixelzzz99 <sherzik99@gmail.com>
2022-09-26 22:19:54 +0500

    end simple api

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {
+    this.moviesService = moviesService;
+  }
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    this.moviesService.remove(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.patch(movieId, updateData);
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +27,85 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    describe('GET', () => {
+      it('should return 200', () => {
+        return request(app.getHttpServer())
+          .get('/movies')
+          .expect(200)
+          .expect([]);
+      });
+    });
+    describe('POST', () => {
+      it('should return 201', () => {
+        return request(app.getHttpServer())
+          .post('/movies')
+          .send({
+            title: 'Test Movie',
+            genres: ['test'],
+            year: 2000,
+          })
+          .expect(201);
+      });
+
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/movies')
+          .send({
+            title: 'Test Movie',
+            genres: ['test'],
+            year: 2000,
+            other: 'thing',
+          })
+          .expect(400);
+      });
+    });
+
+    describe('DELETE', () => {
+      it('should return 404', () => {
+        return request(app.getHttpServer()).delete('/movies').expect(404);
+      });
+    });
+  });
+
+  describe('/movies/:id', () => {
+    describe('GET', () => {
+      it('should return 200', () => {
+        return request(app.getHttpServer()).get('/movies/1').expect(200);
+      });
+
+      it('should return 404', () => {
+        return request(app.getHttpServer()).get('/movies/999').expect(404);
+      });
+    });
+
+    describe('PATCH', () => {
+      it('should return 200', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/1')
+          .send({ title: 'Updated Test' })
+          .expect(200);
+      });
+
+      it('should return 404', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/999')
+          .send({ title: 'Updated Test' })
+          .expect(404);
+      });
+    });
+
+    describe('DELETE', () => {
+      it('should return 200', () => {
+        return request(app.getHttpServer()).delete('/movies/1').expect(200);
+      });
+
+      it('should return 404', () => {
+        return request(app.getHttpServer()).delete('/movies/999').expect(404);
+      });
+    });
   });
 });
